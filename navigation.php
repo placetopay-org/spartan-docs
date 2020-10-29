b784b15db3c28ed4115c81076161cc297f1871da
Alex <alex.henaof@gmail.com>
2020-10-29 16:11:48 -0500

    Ajustes al css para integrar panel, adicionar la fuente Inter, organizar la estructura del sistema de diseño en el menu

--- navigation.php
+++ navigation.php
@@ -10,10 +10,35 @@ return [
             'Custom 404 Page' => 'docs/custom-404-page',
         ],
     ],
-    'Components' => [
-        'url' => 'docs/components',
+    'Estilos' => [
+        'url' => 'docs/estilos',
         'children' => [
-            'Buttons' => 'docs/components/buttons'
+            'Colores' => 'docs/components/colores'
+            'Tipografía' => 'docs/components/tipografia'
+        ]
+    ],
+    'Componentes' => [
+        'url' => 'docs/componentes',
+        'children' => [
+            'Inputs para filtros' => 'docs/componentes/inputs-filtros'
+            'Formularios' => 'docs/componentes/formularios'
+            'Botones' => 'docs/componentes/botones'
+            'Tablas' => 'docs/componentes/tablas'
+            'Modales' => 'docs/componentes/modales'
+            'Autentificación' => 'docs/componentes/autentificacion',
+            'Alertas' => 'docs/componentes/alertas',
+            'Placeholders' => 'docs/componentes/placeholders',
+            'Navegación' => 'docs/componentes/navegacion',
+            'Contenedores' => 'docs/componentes/contenedores',
+            'Vizualización de datos' => 'docs/componentes/visualizacion-de-pagos',
+        ]
+    ],
+    'Íconos e ilustraciones' => [
+        'url' => 'docs/graficos',
+        'children' => [
+            'Íconos' => 'docs/graficos/iconos'
+            'Logos' => 'docs/graficos/logos'
+            'Ilustraciones' => 'docs/graficos/ilustraciones'
         ]
     ],
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
